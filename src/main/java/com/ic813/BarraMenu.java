b6b1c4c31d5ee652803da5107219f98ad6f9d420
fillipedornelas <dornelasfillipe@gmail.com>
2014-07-08 12:52:08 -0300

    Barra de Menu
    Colocar o bootstrap.min.css em /src/main/webapp
    
    e o resto no lugar de sempre.
    
    não consegui fazer a barra de menu pegar em todas páginas pois tem que
    ter o add(link... em todos os documentos, seria meio desnecerrário.
    se alguém conseguir ou tiver alguma solucao avise.

---
+++ src/main/java/com/ic813/BarraMenu.java
@@ -0,0 +1,58 @@
+package com.ic813;
+
+import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.link.Link;
+import org.apache.wicket.request.mapper.parameter.PageParameters;
+
+public class BarraMenu extends WebPage{
+
+	public BarraMenu(final PageParameters parameters) {
+		super(parameters);
+		
+		add(new Link("barraMenu"){
+			@Override
+			public void onClick(){ //mais fácil que tirar doce de criança.
+				
+				setResponsePage(BarraMenu.class);
+			}
+		 });
+		
+		
+		add(new Link("insProjeto"){
+			@Override
+			public void onClick(){ //mais fácil que tirar doce de criança.
+				
+				setResponsePage(CriarProjeto.class);
+			}
+		 });
+		
+		add(new Link("edtProjeto"){
+			@Override
+			public void onClick(){ //mais fácil que tirar 10 nas matérias do rizzo.
+				
+				setResponsePage(EditarProjeto.class);
+			}
+		 });
+		
+		add(new Link("insAgencia"){
+			@Override
+			public void onClick(){ //mais fácil que fazer o cofcof do márcio.
+				
+				setResponsePage(InserirAgencia.class);
+			}
+		 });
+		
+		add(new Link("insColaborador"){
+			@Override
+			public void onClick(){ //mais fácil que lustrar a careca do andré.
+				
+				setResponsePage(InserirColaborador.class);
+			}
+		 });
+		
+		//você não leu os comentários acima.
+		
+	}
+	
+	
+}
