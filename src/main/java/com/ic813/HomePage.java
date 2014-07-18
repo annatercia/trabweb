e8cafdb2b94e385d9390cecb0356853968520584
fillipedornelas <dornelasfillipe@gmail.com>
2014-07-18 18:49:56 -0300

    erro/sucesso CriarProjeto
    o nome é home page mas ela cria um projeto.
    
    mensagem de erro e acerto funcionando...

--- src/main/java/com/ic813/HomePage.java
+++ src/main/java/com/ic813/HomePage.java
@@ -2,7 +2,6 @@ package com.ic813;
 import DAO.*;
 
 import org.apache.wicket.markup.html.form.Form;
-
 import org.apache.wicket.model.PropertyModel;
 import org.apache.wicket.markup.html.form.DropDownChoice;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
@@ -11,12 +10,13 @@ import org.apache.wicket.markup.html.WebPage;
 
 public class HomePage extends WebPage {
 	private static final long serialVersionUID = 1L;
-	
 
 	public HomePage(final PageParameters parameters) {
 		super(parameters);
 		
 		 Form form = new Projeto("formulario"){
+			 
+			 
 		 @Override
 		 
 		 /*
@@ -33,6 +33,8 @@ public class HomePage extends WebPage {
 		  */
 		     protected void onSubmit() {
 		     
+			 PageParameters pp = new PageParameters();
+			 
 		     Projetos p = new Projetos();
 		     p.setTitulo(this.getTituloProjeto());
 		     p.setData_inicio(this.getDataInicio());
@@ -43,16 +45,34 @@ public class HomePage extends WebPage {
 		     p.setValor_financiado(Float.parseFloat(this.getValorFinanciado()));
 		     p.setStatus("em elaboração");
 		     ProjetosDAO pdao= new ProjetosDAO();
-		     if(pdao.Add(p))
-		    	 System.out.print("Foi");
-		     else
-		    	 System.out.print("Nao foi");
+		     if(pdao.Add(p)){
+		    	 
+		    	pp.add("mensagem","Projeto criado com sucesso!"); //necessário para a mensagem de erro
+				setResponsePage(HomePage.class, pp); //necessário para a mensagem de erro
+			    	 
+		     }else{
+		    	pp.add("mensagem","Falha ao criar projeto."); //necessário para a mensagem de erro
+				setResponsePage(HomePage.class, pp); //necessário para a mensagem de erro
+				    	
+		     }
 		     
 		     //System.out.print("Data Inicio: "+this.getDataInicio());
 		     //System.out.print("Data Inicio: "+p.getData_inicio());
 			 //setResponsePage(HomePage.class);
 		 	}
+		 
 		 };
+		 
+		 if(!parameters.isEmpty()){
+			 String oi = getPageParameters().get("mensagem").toString(); //necessário para a mensagem de erro
+			 add(new Label("msgFeedBack",oi)); //necessário para a mensagem de erro
+		 }
+		 else{
+			 add(new Label("msgFeedBack",""));//necessário para a mensagem de erro
+		 }	
+		 
+		 
+		 
 		 add(form);
 
     }
